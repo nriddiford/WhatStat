76acaad7b1270c6ac4155293e468ae1663f21995
Nick Riddiford <nriddiford@users.noreply.github.com>
2018-06-07 15:51:08 +0200

    Delete google-analytics.js

--- shiny/google-analytics.js
+++
@@ -1,6 +0,0 @@
-async src="https://www.googletagmanager.com/gtag/js?id=UA-109758097-1"
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-109758097-1');
